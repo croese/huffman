5440f91d56f8b2eb7360608df1042732be3601a5
Christian Roese <croese@pekininsurance.com>
2017-07-27 14:44:39 -0500

    Code cleanup

--- Huff/Program.cs
+++ Huff/Program.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Huff {
-    class Program {
-        static void Main(string[] args) {
+﻿namespace Huff
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
         }
     }
 }
